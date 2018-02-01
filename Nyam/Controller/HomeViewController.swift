4b966c545b51ff9c9e560977831922840d671d5b
Ade Suluh <ade.suluh@gmail.com>
2018-02-01 16:07:54 +0700

    add futuna

--- Nyam/Controller/HomeViewController.swift
+++ Nyam/Controller/HomeViewController.swift
@@ -8,8 +8,11 @@
 
 import UIKit
 import Pastel
+import Fusuma
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, FusumaDelegate {
+    
+    
 
     @IBOutlet weak var editPhotoButton: UIButton! {
         didSet {
@@ -26,6 +29,16 @@ class HomeViewController: UIViewController {
         }
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        
+        self.navigationController?.navigationBar.isHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        self.navigationController?.navigationBar.isHidden = false
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -52,7 +65,163 @@ class HomeViewController: UIViewController {
         view.insertSubview(pastelView, at: 0)
         
     }
-
     
+    @IBAction func takePhotoForEditing(_ sender: Any) {
+        //show fusuma
+        let fusuma = FusumaViewController()
+        fusuma.delegate = self
+        fusuma.allowMultipleSelection = false
+        //fusuma.availableModes = [.video]
+        fusumaSavesImage = true
+        
+        self.present(fusuma, animated: true, completion: nil)
+    }
+    
+    
+    // MARK: Fusuma Delegate
+    func fusumaImageSelected(_ image: UIImage, source: FusumaMode) {
+        switch source {
+        case .camera:
+            print("Image capture from Camera")
+        case .library:
+            print("Image selected from library")
+        case .video:
+            print("Image taken from Video")
+        default:
+            print("Image selected")
+        }
+        
+        //imageView.image = image CHANGE PLACEHOLDER IMAGE
+        
+        //for next vc
+        self.performSegue(withIdentifier: "testBos", sender: nil)
+    }
+    
+    func fusumaMultipleImageSelected(_ images: [UIImage], source: FusumaMode) {
+        
+        print("Number of selection images: \(images.count)")
+        
+        var count: Double = 0
+        
+        for image in images {
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + (3.0 * count)) {
+                
+                //self.imageView.image = image
+                print("w: \(image.size.width) - h: \(image.size.height)")
+            }
+            count += 1
+        }
+        
+        //self.performSegue(withIdentifier: "testBos", sender: nil)
+        
+    }
+    
+    func fusumaImageSelected(_ image: UIImage, source: FusumaMode, metaData: ImageMetadata) {
+        print("Image mediatype: \(metaData.mediaType)")
+        print("Source image size: \(metaData.pixelWidth)x\(metaData.pixelHeight)")
+        print("Creation date: \(String(describing: metaData.creationDate))")
+        print("Modification date: \(String(describing: metaData.modificationDate))")
+        print("Video duration: \(metaData.duration)")
+        print("Is favourite: \(metaData.isFavourite)")
+        print("Is hidden: \(metaData.isHidden)")
+        print("Location: \(String(describing: metaData.location))")
+    }
+    
+    func fusumaVideoCompleted(withFileURL fileURL: URL) {
+        
+        //print("video completed and output to file: \(fileURL)")
+        //self.fileUrlLabel.text = "file output to: \(fileURL.absoluteString)"
+    }
+    
+    func fusumaDismissedWithImage(_ image: UIImage, source: FusumaMode) {
+        
+        switch source {
+            
+        case .camera:
+            
+            print("Called just after dismissed FusumaViewController using Camera")
+            
+        case .library:
+            
+            print("Called just after dismissed FusumaViewController using Camera Roll")
+            
+        case .video:
+            
+            print("Called just after dismissed FusumaViewController using Video")
+            
+        default:
+            
+            print("Called just after dismissed FusumaViewController")
+        }
+    }
+    
+    func fusumaCameraRollUnauthorized() {
+        
+        print("Camera roll unauthorized")
+        
+        let alert = UIAlertController(title: "Access Requested",
+                                      message: "Saving image needs to access your photo album",
+                                      preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "Settings", style: .default) { (action) -> Void in
+            
+            if let url = URL(string:UIApplicationOpenSettingsURLString) {
+                
+                UIApplication.shared.openURL(url)
+            }
+        })
+        
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel) { (action) -> Void in
+            
+        })
+        
+        guard let vc = UIApplication.shared.delegate?.window??.rootViewController,
+            let presented = vc.presentedViewController else {
+                
+                return
+        }
+        
+        presented.present(alert, animated: true, completion: nil)
+    }
+    
+    func fusumaClosed() {
+        
+        print("Called when the FusumaViewController disappeared")
+    }
+    
+    func fusumaWillClosed() {
+        
+        print("Called when the close button is pressed")
+    }
+
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

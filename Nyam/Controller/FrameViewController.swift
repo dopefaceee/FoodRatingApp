4d81645e276e47fe777becec7ab436daf2ed112c
Ade Suluh <ade.suluh@gmail.com>
2018-02-17 02:29:57 +0700

    uitabbar  & custom view done

--- Nyam/Controller/FrameViewController.swift
+++ Nyam/Controller/FrameViewController.swift
@@ -13,14 +13,25 @@ class FrameViewController: UIViewController {
     @IBOutlet weak var imageToEdit: UIImageView!
     
     @IBOutlet weak var tabBarEditor: UITabBar!
+    
+    @IBOutlet weak var collectionViewFrame: UICollectionView!
    
     var newImage: UIImage!
+    
+    let FrameCellId: String = "FrameCell"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         imageToEdit.image = newImage
         
         tabBarEditor.delegate = self
+        collectionViewFrame.delegate = self
+        collectionViewFrame.dataSource = self
+        
+        tabBarEditor.selectedItem = tabBarEditor.items![0]
+        
+        let nibCell = UINib(nibName: FrameCellId, bundle: nil)
+        collectionViewFrame.register(nibCell, forCellWithReuseIdentifier: FrameCellId)
         
     }
     
@@ -34,17 +45,49 @@ class FrameViewController: UIViewController {
     }
     
     
+    
 }
 
-extension FrameViewController: UITabBarDelegate {
+extension FrameViewController:  UITabBarDelegate, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
     func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
+
+        if item == (tabBar.items as! [UITabBarItem])[0] {
+            print("Index 0 is hitted!")
+        }
         
     }
     
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionViewFrame.dequeueReusableCell(withReuseIdentifier: FrameCellId, for: indexPath) as? FrameCell
+        cell?.labelFrame.text = "Frame1"
+        
+        return cell!
+    }
     
     
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ Nyam/View/FrameCell.swift
@@ -0,0 +1,20 @@
+//
+//  FrameCell.swift
+//  Nyam
+//
+//  Created by Ade Suluh on 16/02/18.
+//  Copyright © 2018 Ade Suluh. All rights reserved.
+//
+
+import UIKit
+
+class FrameCell: UICollectionViewCell {
+    
+    @IBOutlet weak var labelFrame: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
